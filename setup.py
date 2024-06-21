bab90c4a9e18bf523ea2cfd5110ab1fbecd3aa62
Ali Raza <aliraza.ar7785@gmail.com>
2024-06-21 22:14:47 +0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1',
+    author='Ali Raza',
+    author_email='aliraza.ar7785@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
